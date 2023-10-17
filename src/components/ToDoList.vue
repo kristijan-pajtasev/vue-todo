9812f18615ea99708690562196db83a888c660a6
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-18 00:44:46 +0100

    initial todo home page

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,33 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+  },
+  mounted() {
+    console.log("mounted")
+  },
+  data() {
+    return {
+      items: [
+        {text: "1 item", id: 1},
+        {text: "2 item", id: 2},
+        {text: "3 item", id: 3}
+      ]
+    }
+  }
+})
+</script>
+
+<template>
+  <div>todo list</div>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <div>{{item.text}}</div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import ToDoList from "@/components/ToDoList.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ToDoList />
   </main>
 </template>
