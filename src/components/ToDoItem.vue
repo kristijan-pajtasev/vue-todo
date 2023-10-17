f5a81d26b19f867b54364c8bfe73494df1ab40b6
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-18 00:47:41 +0100

    initial item detail component

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,35 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+  },
+  mounted() {
+    console.log("mounted")
+  },
+  data() {
+    return {
+      item: {
+        id: 1,
+        text: "todo text",
+        created: (new Date()).getTime(),
+        updated: (new Date()).getTime(),
+        completed: true
+      }
+    }
+  }
+})
+</script>
+
+<template>
+  <div>item</div>
+  <div>{{item.id}}</div>
+  <div>{{item.text}}</div>
+  <div>{{item.created}}</div>
+  <div>{{item.updated}}</div>
+  <div>{{item.completed}}</div>
+</template>
+
+<style scoped>
+</style>
