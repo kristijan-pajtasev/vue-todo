4bb801b267069bfb07869df1f9b3da6dbe8efa1d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-18 00:50:11 +0100

    item details page

---
+++ src/views/ToDoItemView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import ToDoList from "@/components/ToDoList.vue";
+import ToDoItem from "@/components/ToDoItem.vue";
+</script>
+
+<template>
+  <main>
+    <ToDoItem />
+  </main>
+</template>
