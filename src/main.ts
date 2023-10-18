0cfe3286aa084da5709c72fad89df62d8f959496
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-18 22:01:22 +0100

    store added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import './assets/main.css'
+import {createStore} from 'vuex'
 
-import { createApp } from 'vue'
+const store = createStore({
+    state() {
+        return {}
+    },
+    mutations: {}
+});
+
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(store);
 
 app.mount('#app')
