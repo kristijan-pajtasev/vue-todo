8912f8534fe39d237f0d0ffc757850b03c0f69b2
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-18 21:58:12 +0100

    display date values

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -10,25 +10,27 @@ export default defineComponent({
   },
   data() {
     return {
-      item: {
-        id: 1,
-        text: "todo text",
-        created: (new Date()).getTime(),
-        updated: (new Date()).getTime(),
-        completed: true
-      }
+      id: 1,
+      text: "todo text",
+      created: (new Date()).getTime(),
+      updated: (new Date()).getTime(),
+      completed: true
     }
+  },
+  computed: {
+    displayCreatedDate(){ return new Date(this.created).toLocaleDateString()},
+    displayUpdatedDate(){ return new Date(this.updated).toLocaleDateString()}
   }
 })
 </script>
 
 <template>
   <div>item</div>
-  <div>{{item.id}}</div>
-  <div>{{item.text}}</div>
-  <div>{{item.created}}</div>
-  <div>{{item.updated}}</div>
-  <div>{{item.completed}}</div>
+  <div>{{id}}</div>
+  <div>{{text}}</div>
+  <div>Created: {{displayCreatedDate}}</div>
+  <div>Updated: {{displayUpdatedDate}}</div>
+  <div>{{completed}}</div>
 </template>
 
 <style scoped>
