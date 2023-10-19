2ec74912736eea0248a9892385a5a63cb6cf69ed
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-19 01:26:51 +0100

    init list styling

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -25,12 +25,35 @@ export default defineComponent({
 
 <template>
   <div>todo list</div>
-  <ul>
-    <li v-for="item in items" :key="item.id" @click="goToDetails(item.id)">
-      <div>{{item.text}}</div>
+  <ul class="ToDoList">
+    <li class="ToDoList__itemContainer" v-for="item in items" :key="item.id">
+      <div class="ToDoList__item" @click="goToDetails(item.id)">{{item.text}}</div>
     </li>
   </ul>
 </template>
 
 <style scoped>
+.ToDoList {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  list-style: none;
+  padding: 0;
+  gap: 32px;
+  flex-wrap: wrap;
+  margin: 32px 0 0;
+}
+
+.ToDoList__itemContainer {
+  max-width: calc(50% - 16px);
+  min-width: 25%;
+  height: 150px;
+}
+
+.ToDoList__item {
+  background: #FFFF99;
+  height: 100%;
+  padding: 8px;
+  cursor: pointer;
+}
 </style>
