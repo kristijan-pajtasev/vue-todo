9d92193161948e475dd690d18427671271fab08f
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-19 21:18:43 +0100

    init create todo

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+  },
+  mounted() {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+})
+</script>
+
+<template>
+  <div>create todo</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/CreateToDoView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import ToDoList from "@/components/ToDoList.vue";
+import ToDoItem from "@/components/ToDoItem.vue";
+import CreateTodo from "@/components/CreateTodo.vue";
+</script>
+
+<template>
+  <main>
+    <CreateTodo />
+  </main>
+</template>
