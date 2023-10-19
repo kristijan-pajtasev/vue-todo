d43c79c72a1ee3dca7efb17ab707f369617106c3
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-19 21:19:54 +0100

    clean imports

--- src/views/CreateToDoView.vue
+++ src/views/CreateToDoView.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-import ToDoList from "@/components/ToDoList.vue";
-import ToDoItem from "@/components/ToDoItem.vue";
 import CreateTodo from "@/components/CreateTodo.vue";
 </script>
 
