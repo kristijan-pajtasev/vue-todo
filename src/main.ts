43266fcb7f4871cd6516dcf448e0a4dc6ee61da2
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-21 00:34:56 +0100

    store data in app

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,15 @@ const store = createStore({
                 updated: (new Date()).getTime(),
                 id: `${context.state.items.length + 1}`
             }
+
+            fetch(`${import.meta.env.VITE_FIREBASE_DB_URL}todo.json`, {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(item)
+                }
+            )
             context.state.items = [...context.state.items, item]
         },
         toggleToDoItemStatus(context, payload) {
