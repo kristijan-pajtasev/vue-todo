92fe4e7486b1166532c327689879b330e51029f5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-23 00:19:01 +0100

    login action

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -35,6 +35,26 @@ const UserStore = {
           return data;
         }
       )
+    },
+    logIn(context, payload) {
+      console.log("signUp action", payload)
+      return fetch(`${import.meta.env.VITE_FIREBASE_LOG_IN_AUTH_URL}${import.meta.env.VITE_FIREBASE_API_KEY}`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password
+        })
+      }).then(
+        async res => {
+          const data = await res.json();
+          console.log(data)
+          context.commit("setUserData", data)
+          return data;
+        }
+      )
     }
   }
 }
