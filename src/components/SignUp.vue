ccba4127cffac68a4c008c72b833596cb18f562a
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 00:58:43 +0100

    sign nup styling

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -32,17 +32,56 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>sign up page</div>
-  <form @submit.prevent="submitHandler">
-    <div>
-      <input type="text" placeholder="email" v-model="email" />
-    </div>
-    <div>
-      <input type="text" placeholder="password" v-model="password" />
-    </div>
-    <button>Submit</button>
-  </form>
+  <div class="SignUp">
+    <div class="SignUp__title">Sign Up</div>
+    <form @submit.prevent="submitHandler">
+      <div>
+        <input class="SignUp__input" type="text" placeholder="E-mail" v-model="email" />
+      </div>
+      <div>
+        <input class="SignUp__input" type="password" placeholder="Password" v-model="password" />
+      </div>
+      <button class="SignUp__button">Sign Up</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
+.SignUp {
+  width: 100%;
+  max-width: 480px;
+  margin: 100px auto;
+  /*text-align: center;*/
+}
+
+.SignUp__title {
+  font-weight: bold;
+  color: hsla(160, 100%, 37%, 1);
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.SignUp__input {
+  width: 100%;
+  border: thin solid #ccc;
+  margin-bottom: 0.5rem;
+  border-radius: 0.2rem;
+  padding: 0.4rem 0.8rem;
+  outline: none;
+}
+
+.SignUp__input:focus {
+  border: thin solid #bbb;
+  box-shadow: #eee 2px 2px 5px;
+}
+
+.SignUp__button {
+  width: 100%;
+  padding: 0.4rem 0.8rem;
+  background-color: hsla(160, 100%, 37%, 1);
+  border: none;
+  border-radius: 0.2rem;
+  color: white;
+  cursor: pointer;
+}
 </style>
