e6ed198d5e4bbc672d6a2a3e70112882563494a4
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 01:13:15 +0100

    styling

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -26,12 +26,13 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>
+  <div class="CreateTodo">
+    <h2 class="CreateTodo__title">Create ToDo Item</h2>
     <form @submit.prevent="createTodo">
       <div>
         <label for="todoText">Todo text:</label>
         <div>
-          <textarea id="todoText" v-model="text"></textarea>
+          <textarea class="CreateTodo__input" id="todoText" v-model="text"></textarea>
         </div>
       </div>
       <div>
@@ -40,11 +41,43 @@ export default defineComponent({
         </label>
       </div>
       <div>
-        <button>Create ToDo</button>
+        <button class="CreateTodo__button">Create ToDo</button>
       </div>
     </form>
   </div>
 </template>
 
 <style scoped>
+.CreateTodo {
+  width: 100%;
+  max-width: 480px;
+  margin: 100px auto;
+  /*text-align: center;*/
+}
+
+.CreateTodo__title {
+  font-weight: bold;
+  color: hsla(160, 100%, 37%, 1);
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.CreateTodo__input {
+  width: 100%;
+  border: thin solid #ccc;
+  margin-bottom: 0.5rem;
+  border-radius: 0.2rem;
+  padding: 0.4rem 0.8rem;
+  outline: none;
+}
+
+.CreateTodo__button {
+  width: 100%;
+  padding: 0.4rem 0.8rem;
+  background-color: hsla(160, 100%, 37%, 1);
+  border: none;
+  border-radius: 0.2rem;
+  color: white;
+  cursor: pointer;
+}
 </style>
